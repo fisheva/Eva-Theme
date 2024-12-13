b78f566446ca99cd904eda470b3078ec9b570a8d
fisheva <fisheva@foxmail.com>
2024-12-13 19:30:51 +0800

    2.7.4

---
+++ scopes.js
@@ -0,0 +1,12 @@
+let scopes = {
+    "keyword.operator.new": {
+        name:'',
+        color: { light: "#7C4DFF", dark: "#A78CFA" },
+        fontStyle: {
+            normal: "",
+            italic: "italic",
+            bold: "bold",
+            italicBold: "italic bold",
+        },
+    },
+};
